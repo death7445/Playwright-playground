fb548529bbf06116a830ffdf4db160e621b77e91
death7445 <61997677+death7445@users.noreply.github.com>
2025-07-01 12:03:09 -0600

    Delete tests directory

--- tests/test.spec.ts
+++
@@ -1,7 +0,0 @@
-
-import { test, expect } from '@playwright/test';
-
-test.only('connect to leagueify.org', async ({ page }) => {
-  await page.goto('https://leagueify.org/');
-  await expect(page).toHaveTitle(/Leagueify/);
-});
