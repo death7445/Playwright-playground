ff662974bc330d5c36f458d329aad37435e11b2e
Robert Elmore <Robertelmorebs2023@gmail.com>
2025-07-01 12:18:01 -0600

    testing

---
+++ .github/workflows/leagueify-test.ts
@@ -0,0 +1,10 @@
+
+import {
+    test, expect
+} 
+from '@playwright/test';
+
+test.only('connect to leagueify.org', async ({ page }) => {
+    await page.goto('https://leagueify.org');
+    await expect(page).toHaveTitle(/Leagueify/);
+});
